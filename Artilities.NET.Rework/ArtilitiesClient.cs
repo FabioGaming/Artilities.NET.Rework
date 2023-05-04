54c659c2e5e55ecb1af398a3a4662e034d336a64
Fabio Gaming <fabio.furch@gmail.com>
2023-05-04 19:51:37 +0200

    added base

---
+++ Artilities.NET.Rework/ArtilitiesClient.cs
@@ -0,0 +1,18 @@
+﻿namespace Artilities.NET.Rework
+{
+    public class ArtilitiesClient
+    {
+        public string APIKey { get; private set; }
+        public string discordId { get; private set; }
+
+
+        public ArtilitiesClient(string apiKey, string discordId )
+        {
+            APIKey = apiKey;
+            this.discordId = discordId;
+        }
+
+
+        public 
+    }
+}
