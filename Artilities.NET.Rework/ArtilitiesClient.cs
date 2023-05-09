20941b3e69212b691611fef469dd001011c7cef8
Fabio Gaming <fabio.furch@gmail.com>
2023-05-09 12:14:35 +0200

    did stuff
    yems

--- Artilities.NET.Rework/ArtilitiesClient.cs
+++ Artilities.NET.Rework/ArtilitiesClient.cs
@@ -1,18 +1,40 @@
-﻿namespace Artilities.NET.Rework
+﻿using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace Artilities
 {
     public class ArtilitiesClient
     {
         public string APIKey { get; private set; }
         public string discordId { get; private set; }
 
+        public bool ignoreNonOK { get; set; } = false;
 
-        public ArtilitiesClient(string apiKey, string discordId )
+        private HttpClient client;
+        public Dictionary<string, string> headers;
+
+        public ArtilitiesClient(string apiKey = null, string discordId = null)
         {
+            Version libVersion = Assembly.GetExecutingAssembly().GetName().Version;
             APIKey = apiKey;
             this.discordId = discordId;
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://artilities-web-api.vercel.app");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", $"Artilities.NET@{libVersion.Major}.{libVersion.Minor}.{libVersion.Revision}");
+            headers = client.DefaultRequestHeaders.ToDictionary<string,string>();
+            
+            
         }
 
 
-        public 
+        public void GetIdea()
+        {
+            try
+            {
+
+            }catch(Exception e) { throw new Exception(e.Message); }
+        }
     }
 }
---
+++ Artilities.NET.Rework/Objects/Idea.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artilities.Objects
+{
+    public class DefaultResponse
+    {
+        public int statusCode { get; }
+        public int executionTime { get; }
+        public string english { get; }
+        public string russian { get; }
+        public string german { get; }
+
+        public DefaultResponse(JObject json)
+        {
+
+        }
+    }
+}
